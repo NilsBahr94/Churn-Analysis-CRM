0618c94c726e0eab1dce25f2993fbd08127c3dc3
NilsB <36771170+NilsBMo@users.noreply.github.com>
2018-12-05 18:21:35 +0100

    Update

--- churn_script.R
+++ churn_script.R
@@ -5,7 +5,7 @@ install.packages("tidyverse")
 install.packages("data.table")
 install.packages("ggplot2")
 install.packages("caret")
-install.packages("lubridate")
+install.packages("lubridate"))
 
 # Load Packages ------------------------------------------------------------
 
@@ -16,7 +16,7 @@ require(ggplot2)
 require(caret)
 require(lubridate)
 
-# Import Data I -------------------------------------------------------------
+# Import 2017 Data -------------------------------------------------------------
 
 data_2017 = read_excel("Data\\Data January 2017.xlsx")
 # write.csv(data_2017, "Data_January_2017.csv")
@@ -26,19 +26,15 @@ data = fread("Data\\Data_January_2017.csv", na.strings = c("-", "NA"))
 # Replace "-" by NA
 data[data == "-"] = NA
 
-#Convert data into right types
-data$DBII = as.numeric(data$DBII) # Warning as output
+#Convert features into right data types
+data$V1 = as.character(data$V1)
+data$`Zip code` = as.character(data$`Zip code`)
+data$DBII = as.numeric(data$DBII)
 data$Contract_ID = as.character(data$Contract_ID)
 data$Age = as.integer(data$Age)
 data$`Customer since` = ymd(data$`Customer since`)
 data$`Contract start date` = ymd(data$`Contract start date`)
-data$Consumption = as.numeric(data$Consumption) # Take care
-
-# Check whether new NA's have been introduced by data type conversion
-sum(is.na(data$DBII))
-
-
-
+data$Consumption = as.numeric(data$Consumption) 
 
 
 # Explore Data I ----------------------------------------------------------
@@ -46,20 +42,36 @@ sum(is.na(data$DBII))
 str(data)
 summary(data)
 
-
 # Clean Data --------------------------------------------------------------
 
-# NA's instead of "-"
-
-
-# Detect NA's per feature
+# Detect Percentage of NA's per feature
 
 apply(data, 2, function(col)sum(is.na(col))/length(col))
 
 
+# Data Preparation --------------------------------------------------------
+
+# Customers older than 105 years
 
 
-# Test set ----------------------------------------------------------------
+# Modeling ----------------------------------------------------------------
+
+myGrid = data.frame()
+
+myControl = trainControl(method = "cv", number = 10, summaryFunction = twoClassSummary)
+
+model = train(churn ~., data = data, method = "xgbLinear", 
+              trControl = myControl, tuneGrid = myGrid
+              )
+
+plot(model)
+
+# Model Evaluation --------------------------------------------------------
+
+confusionMatrix()
+
+
+# Import 2018 Data ----------------------------------------------------------------
 
 data_2018 = read_excel("Data\\Data November 2018.xlsx")
 # write.csv(data_2018, "Data_November_2018.csv")
