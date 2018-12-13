2e4c3adff367eaf96689071cee8a74ca48d3f670
JulianWI1221 <hentschel_juli@hotmail.de>
2018-12-13 16:56:03 +0100

    Update churn_script.R

--- churn_script.R
+++ churn_script.R
@@ -20,14 +20,34 @@ require(lubridate)
 require(dbscan)
 require(mice)
 require(stringr)
+require(mlr)
 
 # Import 2017 Data -------------------------------------------------------------
 
 # N
 data = fread("Data\\Data_January_2017_2.csv", na.strings = c("-", "NA"))
+#setwd("D:/Julian Hentschel/CRMADM/Churn-Analysis-CRM")
 
 #remove title, V1, Contract_ID, customer_since, Notice_period and automatic_contract_extension from the data set
-data = data[,c("Contract_ID","Client type", "Zip code", "Age", "Duration of customer relationship", "Contract start date", "Minimum contract term", "Consumption", "Payment on account","Annual account", "Bill shock","Online account", "Opt In Mail", "Opt In Post", "Opt In Tel", "Market area","Recovered","DBII", "Churn")]
+data = data[,c("Contract_ID", 
+               "Client type", 
+               "Zip code", 
+               "Age", 
+               "Duration of customer relationship", 
+               "Contract start date", 
+               "Minimum contract term", 
+               "Consumption", 
+               "Payment on account", 
+               "Annual account", 
+               "Bill shock", 
+               "Online account", 
+               "Opt In Mail", 
+               "Opt In Post", 
+               "Opt In Tel", 
+               "Market area", 
+               "Recovered", 
+               "DBII", 
+               "Churn")]
 
 #rename cols in order to avoid problems with imputation
 names(data)[names(data) == 'Client type'] <- 'Client_type'
@@ -69,15 +89,17 @@ data = data[,-c("Contract start date", "Market area")]
 
 #Convert features into right data types
 data$`Zip_code` = as.character(data$`Zip_code`)
-data$DBII = as.numeric(gsub(",", ".", gsub("\\.", "", data$DBII)))
-data$`Annual_account` = as.numeric(gsub(",", ".", gsub("\\.", "", data$`Annual_account`)))
 data$Contract_ID = as.character(data$Contract_ID)
+
 data$Age = as.integer(data$Age)
 
+data$DBII = as.numeric(gsub(",", ".", gsub("\\.", "", data$DBII)))
+data$`Annual_account` = as.numeric(gsub(",", ".", gsub("\\.", "", data$`Annual_account`)))
+data$Payment_on_account = as.numeric(data$Payment_on_account)
 data$Consumption = as.numeric(data$Consumption) 
+
 data$Churn = as.factor(data$Churn)
 data$Recovered = as.factor(data$Recovered)
-
 data$`Client_type` = as.factor(data$`Client_type`)
 data$`Bill_shock` = as.factor(data$`Bill_shock`)
 data$`Opt_In_Mail` = as.factor(data$`Opt_In_Mail`)
@@ -91,6 +113,28 @@ data$`Restlich` = as.factor(data$`Restlich`)
 
 imp <- mice(data)
 
+test <- complete(imp)
+
+summary(test)
+
+# Normalize data ----------------------------------------------------------
+
+
+
+test$Payment_on_account = as.numeric(test$Payment_on_account)
+
+normTest = normalizeFeatures(test, cols = c("Age",
+                                            "Payment_on_account", 
+                                            "Duration_of_customer_relationship", 
+                                            "Minimum_contract_term", 
+                                            "Consumption", 
+                                            "Annual_account", 
+                                            "DBII", 
+                                            "Contract_since"))
+
+churner_df = normTest[normTest$Churn==1,]
+
+summary(churner_df)
 
 # Explore Data I ----------------------------------------------------------
 
@@ -112,11 +156,59 @@ plot(data[, Consumption])
 
 max(data$Consumption)
 
+r = data("moons")
 
-#Cluster Analysis
-hdb <- hdbscan(data, minPts = 4)
 
-unique(data$Title)
+# Cluster Analysis ----------------------------------------------------------
+#Select subset of features (namely all except for client_ID and Zip_code)
+cluster_df = (churner_df[,c("Client_type", 
+                            "Age", 
+                            "Duration_of_customer_relationship", 
+                            "Minimum_contract_term", 
+                            "Consumption", 
+                            "Payment_on_account", 
+                            "Annual_account", 
+                            "Bill_shock", 
+                            "Online_account",
+                            "Opt_In_Mail", 
+                            "Opt_In_Post", 
+                            "Opt_In_Tel", 
+                            "Recovered", 
+                            "DBII", 
+                            "Contract_since", 
+                            "International", 
+                            "Grundversorger", 
+                            "Erweitert", 
+                            "Restlich")])
+
+#Transform to numeric features
+cluster_df$Client_type = as.numeric(cluster_df$Client_type)
+cluster_df$Age = as.numeric(cluster_df$Age)
+cluster_df$Duration_of_customer_relationship = as.numeric(cluster_df$Duration_of_customer_relationship)
+cluster_df$Minimum_contract_term = as.numeric(cluster_df$Minimum_contract_term)
+cluster_df$Consumption = as.numeric(cluster_df$Consumption)
+cluster_df$Payment_on_account = as.numeric(cluster_df$Payment_on_account)
+cluster_df$Annual_account = as.numeric(cluster_df$Annual_account)
+cluster_df$Bill_shock = as.numeric(cluster_df$Bill_shock)
+cluster_df$Online_account = as.numeric(cluster_df$Online_account)
+cluster_df$Opt_In_Mail = as.numeric(cluster_df$Opt_In_Mail)
+cluster_df$Opt_In_Post = as.numeric(cluster_df$Opt_In_Post)
+cluster_df$Opt_In_Tel = as.numeric(cluster_df$Opt_In_Tel)
+cluster_df$Recovered = as.numeric(cluster_df$Recovered)
+cluster_df$DBII = as.numeric(cluster_df$DBII)
+cluster_df$Contract_since = as.numeric(cluster_df$Contract_since)
+cluster_df$International = as.numeric(cluster_df$International)
+cluster_df$Grundversorger = as.numeric(cluster_df$Grundversorger)
+cluster_df$Erweitert = as.numeric(cluster_df$Erweitert)
+cluster_df$Restlich = as.numeric(cluster_df$Restlich)
+
+#Execute HDBSCAN clustering algorithm
+hdb <- hdbscan(cluster_df, minPts = 4)
+
+plot(hdb)
+
+result = cbind(test[test$Churn==1,], hdb$cluster)
+
 
 # Payment on Account
 # Annual Account
