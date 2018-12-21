b1bcefe03e38d7f72b97b205386b0f2cc26d188a
Lina Oechsner <linaoechsner@linas-mbp.speedport.ip>
2018-12-21 10:52:49 +0100

    Synthesis both codes

--- churn_script.R
+++ churn_script.R
@@ -18,47 +18,95 @@ require(lubridate)
 
 # Import 2017 Data -------------------------------------------------------------
 
-#n
-
 # data_2017 = read_excel("Data\\Data January 2017.xlsx")
 # write.csv(data_2017, "Data_January_2017.csv")
 
 # N
 data = fread("Data\\Data_January_2017.csv", na.strings = c("-", "NA"))
 
-# l
-
-data_2017 = read_excel("Data/Data January 2017.xlsx")
-write.csv(data_2017, "Data_January_2017.csv")
-
+# L
 data = fread("Data/Data_January_2017.csv", na.strings = c("-", "NA"))
 
+#remove title, V1, customer_since, Notice_period and automatic_contract_extension from the data set
+data = data[,c("Contract_ID", 
+               "Client type", 
+               "Zip code", 
+               "Age", 
+               "Duration of customer relationship", 
+               "Contract start date", 
+               "Minimum contract term", 
+               "Consumption", 
+               "Payment on account", 
+               "Annual account", 
+               "Bill shock", 
+               "Online account", 
+               "Opt In Mail", 
+               "Opt In Post", 
+               "Opt In Tel", 
+               "Market area", 
+               "Recovered", 
+               "DBII", 
+               "Churn")]
+
+#rename cols in order to avoid problems with imputation
+names(data)[names(data) == 'Client type'] <- 'Client_type'
+names(data)[names(data) == 'Zip code'] <- 'Zip_code'
+names(data)[names(data) == 'Duration of customer relationship'] <- 'Duration_of_customer_relationship'
+names(data)[names(data) == 'Minimum contract term'] <- 'Minimum_contract_term'
+names(data)[names(data) == 'Payment on account'] <- 'Payment_on_account'
+names(data)[names(data) == 'Annual account'] <- 'Annual_account'
+names(data)[names(data) == 'Bill shock'] <- 'Bill_shock'
+names(data)[names(data) == 'Online account'] <- 'Online_account'
+names(data)[names(data) == 'Opt In Mail'] <- 'Opt_In_Mail'
+names(data)[names(data) == 'Opt In Post'] <- 'Opt_In_Post'
+names(data)[names(data) == 'Opt In Tel'] <- 'Opt_In_Tel'
 
 # Replace "-" by NA
 data[data == "-"] = NA
 
-#Convert features into right data types
-data$V1 = as.character(data$V1)
-data$Title = as.factor(data$Title)
-data$`Zip code` = as.character(data$`Zip code`)
-data$DBII = as.numeric(data$DBII)
-data$Contract_ID = as.character(data$Contract_ID)
-data$Age = as.integer(data$Age)
-data$`Customer since` = ymd(data$`Customer since`)
-data$`Contract start date` = ymd(data$`Contract start date`)
-data$Consumption = as.numeric(data$Consumption) 
-data$Churn = as.factor(data$Churn)
-
-data$`Client type` = as.factor(data$`Client type`)
-data$`Bill shock` = as.factor(data$`Bill shock`)
-data$`Opt In Mail` = as.factor(data$`Opt In Mail`)
-data$`Opt In Post` = as.factor(data$`Opt In Post`)
-data$`Opt In Tel` = as.factor(data$`Opt In Tel`)
-
 # Online Account - NA to 0 
+data$`Online_account`[is.na(data$`Online_account`)] = 0
 
-setnames(data, `Client type`, Client_type)
+# Recovered - "" to 0 and "X" to 1
+data$Recovered[data$`Recovered`!="X"] = 0
+data$Recovered[data$`Recovered`=="X"] = 1
 
+#Transform "Contract start date" to number of months
+data$`Contract start date` = dmy(data$`Contract start date`, locale = "English")
+data$`Contract_since` = unlist(lapply(data$`Contract start date`, FUN = function(x) interval(ymd(x),ymd('20170131')) %/% months(1)))
+
+#Transform "Market area" to binary variables
+data$`Grundversorger` = unlist(lapply(data$`Market area`, FUN = function(x) ifelse(x=="Grundversorger",1,0) ))
+data$`Erweitert` = unlist(lapply(data$`Market area`, FUN = function(x) ifelse(x=="Erweitertes Netzgebiet",1,0) ))
+data$`Restlich` = unlist(lapply(data$`Market area`, FUN = function(x) ifelse(x=="Restliches Bundesgebiet",1,0) ))
+data = data[,-c("Contract start date", "Market area")]
+
+#Convert features into right data types
+data$Contract_ID = as.character(data$Contract_ID)
+data$`Zip_code` = as.character(data$`Zip_code`)
+data$`Client_type` = as.factor(data$`Client_type`)
+data$Age = as.integer(data$Age)
+data$Consumption = as.numeric(data$Consumption) 
+data$Payment_on_account = as.numeric(data$Payment_on_account)
+data$`Annual_account` = as.numeric(gsub(",", ".", gsub("\\.", "", data$`Annual_account`)))
+data$`Bill_shock` = as.factor(data$`Bill_shock`)
+data$`Online_account` = as.factor(data$`Online_account`)
+data$`Opt_In_Mail` = as.factor(data$`Opt_In_Mail`)
+data$`Opt_In_Post` = as.factor(data$`Opt_In_Post`)
+data$`Opt_In_Tel` = as.factor(data$`Opt_In_Tel`)
+data$`Grundversorger` = as.factor(data$`Grundversorger`)
+data$`Erweitert` = as.factor(data$`Erweitert`)
+data$`Restlich` = as.factor(data$`Restlich`)
+data$Recovered = as.factor(data$Recovered)
+data$Churn = as.factor(data$Churn)
+data$DBII = as.numeric(gsub(",", ".", gsub("\\.", "", data$DBII)))
+
+# Feature engineering -------------------------------------------------------------
+
+#Create feature "international"
+data$`International` = unlist(gregexpr("[0-9]{5}", data$`Zip_code`))
+data$`International`[data$`International` == 1] = 0
+data$`International`[data$`International` == -1] = 1
 
 # Explore Data I ----------------------------------------------------------
 
