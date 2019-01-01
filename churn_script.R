dd2a63a19fe05a25dae575ba8e639f8177b5a107
Lina Oechsner <linaoechsner@linas-mbp.speedport_w_724v_typ_a_05011603_06_001>
2019-01-01 19:34:35 +0100

    correct import

--- churn_script.R
+++ churn_script.R
@@ -97,17 +97,16 @@ library(h2o)
 # write.csv(data_2017, "Data\\Data_January_2017.csv")
 
 # L
-# data_2017 = read_excel("Data/Data January 2017.xlsx", na = "-", col_types = c("text","guess","guess","text","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess"))
+# data_2017 = read_excel("Data/Data January 2017.xlsx", na = "-", col_types = c("text","guess","guess","text","guess","guess","guess","guess","guess","guess","guess","numeric","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess","guess"))
 # write.csv2(data_2017, "Data/Data_January_2017_3.csv")
 
 # L
-original_data = fread("Data/Data_January_2017_3.csv", na.strings = "NA", dec = ",")
-data = original_data
+data = fread("Data/Data_January_2017_3.csv", na.strings = "NA", dec = ",")
+
 
 # N
-original_data = fread("Data\\Data_January_2017_3.csv", na.strings = "NA", dec = ",")
-data = original_data
-  
+data = fread("Data\\Data_January_2017_3.csv", na.strings = "NA", dec = ",")
+
   #remove title and V1 from the data set
   data = data[,c("Contract_ID", 
                  "Client type", 
